91f0c29ab7c4be08f8609dddc04819ae86428c32
dydr31 <84735554+dydr31@users.noreply.github.com>
2024-11-02 19:13:19 +0300

    added createHashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header';
 import { HomePage } from './components/HomePage';
 import { ProjectsPage } from './components/ProjectsPage';
 import { ErrorPage } from './components/ErrorPage';
 
-const router = createBrowserRouter([{
-  path: '/',
+const router = createHashRouter([{
+  path: '/*',
   element: <Header/>,
   errorElement: <ErrorPage/>,
   children: [
     {
-       path: '/',
+       path: '/*',
        element: <HomePage/>
     },
     {
